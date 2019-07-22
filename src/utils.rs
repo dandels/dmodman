138656d07f96252f3b739efd3670f1c37ef4f118
dandels <dandels.sh@gmail.com>
2019-07-22 21:03:07 +0300

    Finalize md5 hash checking for a file

--- src/utils.rs
+++ src/utils.rs
@@ -7,18 +7,15 @@ pub fn file_name_from_url(url: &Url) -> String {
     let path_segments = url.path_segments().unwrap();
     let encoded = path_segments.last().unwrap();
     let decode = percent_encoding::percent_decode(encoded.as_bytes());
-    let file_name = decode.decode_utf8_lossy().to_ascii_lowercase();
+    let file_name = decode.decode_utf8_lossy().to_string();
     file_name
 }
 
 pub fn md5sum(path: &PathBuf) -> Result<String, std::io::Error> {
     let mut file = File::open(path)?;
     let mut hasher = Md5::new();
-    let n = std::io::copy(&mut file, &mut hasher);
+    let _bytes_read = std::io::copy(&mut file, &mut hasher);
     let hash = hasher.result();
-    println!("Path: {:?}", &path);
-    println!("Bytes processed: {:?}", n);
-    println!("Hash value: {:x}", hash);
     Ok(format!("{:x}", hash))
 }
 
