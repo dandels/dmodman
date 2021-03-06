d576435ba9e58015bc760f427e9c41754b541383
dandels <dandels.sh@gmail.com>
2021-03-06 22:40:02 +0200

    Rename function for clarity

--- src/db/cacheable.rs
+++ src/db/cacheable.rs
@@ -8,7 +8,7 @@ use std::path::PathBuf;
 pub trait Cacheable: Serialize + DeserializeOwned {
     const CACHE_DIR_NAME: &'static str;
 
-    fn cache_dir(game: &str, mod_id: &u32) -> PathBuf {
+    fn cache_file(game: &str, mod_id: &u32) -> PathBuf {
         let mut path = config::cache_dir(&game);
         path.push(Self::CACHE_DIR_NAME);
         path.push(format!("{}.json", &mod_id.to_string()));
@@ -17,7 +17,7 @@ pub trait Cacheable: Serialize + DeserializeOwned {
 
     fn save_to_cache(&self, game: &str, mod_id: &u32) -> Result<(), Error> {
         let data = serde_json::to_string_pretty(&self)?;
-        let path = Self::cache_dir(game, mod_id);
+        let path = Self::cache_file(game, mod_id);
         std::fs::create_dir_all(path.parent().unwrap().to_str().unwrap())?;
         let mut file = File::create(&path)?;
         file.write_all(data.as_bytes())?;
@@ -26,7 +26,7 @@ pub trait Cacheable: Serialize + DeserializeOwned {
 
     // TODO get rid of the mod id here to support more query types
     fn try_from_cache(game: &str, mod_id: &u32) -> Result<Self, Error> {
-        let path = Self::cache_dir(game, mod_id);
+        let path = Self::cache_file(game, mod_id);
         let contents = std::fs::read_to_string(path)?;
         let ret = serde_json::from_str(&contents)?;
         Ok(ret)
