1b1cd76f32f57d80608a78e30acd5165f8e6dcd7
dandels <dandels.sh@gmail.com>
2022-01-22 20:16:34 +0200

    Update clap and dirs libraries

--- src/cmd.rs
+++ src/cmd.rs
@@ -4,13 +4,13 @@ pub const ARG_GAME: &str = "game";
 pub const ARG_UNNAMED: &str = "nxm_url";
 pub const VAL_GAME: &str = "GAME";
 
-pub fn args() -> ArgMatches<'static> {
+pub fn args() -> ArgMatches {
     let matches: ArgMatches = App::new(clap::crate_name!())
         .version(clap::crate_version!())
         .about("A third-party command line frontend to the Nexusmods API.")
         .arg(
             Arg::with_name(ARG_GAME)
-                .short("g")
+                .short('g')
                 .long(ARG_GAME)
                 .value_name(VAL_GAME)
                 .help("The game to manage. Required if the default game is not configured."),
