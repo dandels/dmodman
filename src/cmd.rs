e7fa23935e00307489aeb600257808d1e085dd33
dandels <dandels.sh@gmail.com>
2022-01-22 21:02:14 +0200

    Fix deprecated syntax warning from clap

--- src/cmd.rs
+++ src/cmd.rs
@@ -9,14 +9,14 @@ pub fn args() -> ArgMatches {
         .version(clap::crate_version!())
         .about("A third-party command line frontend to the Nexusmods API.")
         .arg(
-            Arg::with_name(ARG_GAME)
+            Arg::new(ARG_GAME)
                 .short('g')
                 .long(ARG_GAME)
                 .value_name(VAL_GAME)
                 .help("The game to manage. Required if the default game is not configured."),
         )
         .arg(
-            Arg::with_name(ARG_UNNAMED)
+            Arg::new(ARG_UNNAMED)
                 .value_name("nxm_url")
                 .help("A nxm:// url to download."),
         )
