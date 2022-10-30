6b4ba1dccafda36915e89977ca9f9f31daedb324
dandels <dandels.sh@gmail.com>
2022-10-30 20:26:13 +0200

    Fix DownloadLink to use Queriable trait

--- src/api/query/download_links.rs
+++ src/api/query/download_links.rs
@@ -1,6 +1,5 @@
-use crate::api::error::RequestError;
-use crate::api::Client;
-use crate::util::format;
+use super::Queriable;
+use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -16,13 +15,7 @@ pub struct Location {
     pub URI: String,
 }
 
-impl DownloadLinks {
+#[async_trait]
+impl Queriable for DownloadLinks {
     const FORMAT_STRING: &'static str = "games/{}/mods/{}/files/{}/download_link.json?{}";
-
-    pub async fn request(client: &Client, params: Vec<&str>) -> Result<DownloadLinks, RequestError> {
-        let endpoint = format::vec_with_format_string(Self::FORMAT_STRING, params);
-        let resp = client.send_api_request(&endpoint).await?.error_for_status()?;
-        let ret: Vec<Location> = serde_json::from_value(resp.json().await?).unwrap();
-        Ok(DownloadLinks { locations: ret })
-    }
 }
