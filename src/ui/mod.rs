362a6edba1612393fb4cf114cb00aff85d909bff
dandels <dandels@hotmail.com>
2024-03-04 20:25:19 +0200

    Merge event.rs into the main module to reduce source tree clutter

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,23 +1,28 @@
 mod component;
-mod event;
 mod hotkeys;
 mod main_ui;
 mod navigation;
 mod rectangles;
 pub mod sso;
 
-use std::error::Error;
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::Arc;
-
 pub use main_ui::*;
-use signal_hook::consts::signal::*;
-use signal_hook_tokio::Signals;
+
 use ratatui::backend::{Backend, TermionBackend};
 use ratatui::Terminal;
+use signal_hook::consts::signal::*;
+use signal_hook_tokio::Signals;
+use std::error::Error;
+use std::io;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use std::thread;
+use std::time::Duration;
+use termion::event::Event;
 use termion::input::MouseTerminal;
+use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 use termion::screen::IntoAlternateScreen;
+use tokio::sync::mpsc;
 use tokio_stream::StreamExt;
 
 pub fn term_setup() -> Result<Terminal<impl Backend>, Box<dyn Error>> {
@@ -36,5 +41,46 @@ pub async fn handle_sigwinch(is_window_resized: Arc<AtomicBool>) {
     let mut signals = Signals::new([SIGWINCH]).unwrap();
     while signals.next().await.is_some() {
         is_window_resized.store(true, Ordering::Relaxed);
+    }
+}
+
+pub enum TickEvent {
+    Input(Event),
+    Tick,
+}
+
+pub struct Events {
+    rx: mpsc::UnboundedReceiver<TickEvent>,
+}
+
+impl Events {
+    pub fn new() -> Events {
+        let tick_rate = Duration::from_millis(250);
+
+        let (tx, rx) = mpsc::unbounded_channel();
+        let _input_handle = {
+            let tx = tx.clone();
+            thread::spawn(move || {
+                let stdin = io::stdin();
+                for event in stdin.events().flatten() {
+                    if tx.send(TickEvent::Input(event)).is_err() {
+                        return;
+                    }
+                }
+            })
+        };
+        let _tick_handle = {
+            thread::spawn(move || loop {
+                if tx.send(TickEvent::Tick).is_err() {
+                    break;
+                }
+                thread::sleep(tick_rate);
+            })
+        };
+        Events { rx }
+    }
+
+    pub async fn next(&mut self) -> Option<TickEvent> {
+        self.rx.recv().await
     }
 }
